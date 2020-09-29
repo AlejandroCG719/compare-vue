563789b602189a88bf7dcf05c7f0b29e53889f55
Alejandro CG <ceron.guzman.alejandro@gmail.com>
2020-09-29 16:50:56 -0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,102 @@
+ <template>
+  <div class="Home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ materia }}</h1>
+          <h1 class="title">
+            {{ heading }}
+          </h1>
+          <div class="is-two-thirds columns is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading}}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn" href="">Learn more</a>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-third desktop">
+            <p class="title"><strong>We provide superior logistcs so that your bussiness can succed in a crazy worl </strong></p>
+          </div>
+          <div class="column">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet posuere tortor, id aliquet erat. Duis in ultricies dui. Donec fringilla nulla sollicitudin rutrum accumsan. Cras sed aliquet justo, in varius est. Nulla rhoncus fermentum leo non consequat. Curabitur quis eros eu nisl mollis dignissim.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">-Gary simon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data (){
+    return {
+      heading : 'Searing to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet posuere tortor, id aliquet erat. Duis in ultricies dui. Donec fringilla nulla sollicitudin rutrum accumsan',
+      materia: 'Vue'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+@import "../mq"
+.hero
+  background: url('../assets/logo.png')
+  background-size: cover
+
+  .title
+   +mobile
+     font-weight: bold
+   +tablet
+     font-size: 2.5rem
+   +desktop
+     font-size: 4rem
+     margin-top: 2rem
+h2
+    margin: 1.5rem 0 2rem 0 !important
+.fa-cog
+    font-size: 40px
+#learn
+  +desktop
+    margin-bottom: 2rem
+.pd
+  +tablet
+    padding: 2em 0
+</style>
